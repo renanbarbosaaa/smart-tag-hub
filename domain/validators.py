4ef16ff4137cea50afc390e4a10949a7f9fcde7c
renanbarbosaaa <renanbarbossilvaa@gmail.com>
2026-09-13 22:51:21 -0300

    refactor: slug validating process aiming clean code and TDD

--- domain/validators.py
+++ domain/validators.py
@@ -1,11 +1,5 @@
-slug = "aX7b9Y1"
+slug = "123456"
 
 def is_valid_slug(slug: str):
-    condicao_de_tamanho = len(slug)
-    condicao_alfanumerica = slug.isalnum()
 
-    if (condicao_de_tamanho == 6) and condicao_alfanumerica == True:
-        return True
-    else:
-        return False
-    
+    return len(slug) == 6 and slug.isalnum()
