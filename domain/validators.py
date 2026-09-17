ef2d60709a8ea9223cdbc3a960c2be91a95c3b3e
renanbarbosaaa <renanbarbossilvaa@gmail.com>
2026-09-16 21:52:49 -0300

    feat: adding the path validator for sucess api calls

--- domain/validators.py
+++ domain/validators.py
@@ -1,20 +1,16 @@
 from urllib.parse import urlparse
 
-slug = "123456"
-
-
 def is_valid_slug(slug: str):
 
     return len(slug) == 6 and slug.isalnum()
 
-url = "https://www.pathto.com.br"
-
 scheme = {"http", "https"}
 
 def is_valid_url(url: str):
+    
     parsed_url = urlparse(url)
 
     return (
-        parsed_url.scheme in scheme and bool(parsed_url.netloc) 
+        parsed_url.scheme in scheme and bool(parsed_url.netloc) and bool(parsed_url.path) 
         )
     
